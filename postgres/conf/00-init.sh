cfe3dfbe0a2e6c205af7b75e5d6b2feb9c598479
noyuno <noyuno@users.noreply.github.com>
2018-10-20 02:10:54 +0900

    Update

--- postgres/conf/00-init.sh
+++ postgres/conf/00-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-psql -U postgres -c \
-    "create role gitbucket login password '${POSTGRES_GITBUCKET_PASSWORD}'; \
-    create database gitbucket; \
-    grant all privileges on database gitbucket to gitbucket;"
+psql -U postgres -c "drop role if exists gitbucket;"
+psql -U postgres -tc "select 1 from user where usename = 'gitbucket'"        | grep -q 1 || psql -U postgres -c "create role gitbucket login password '${POSTGRES_GITBUCKET_PASSWORD}';"
+psql -U postgres -tc "select 1 from pg_database where datname = 'gitbucket'" | grep -q 1 || psql -U postgres -c "create database gitbucket;"
+psql -U postgres -c "grant all privileges on database gitbucket to gitbucket;"
 
