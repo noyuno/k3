6dff65c27ff92aef4b44f09e4d823087abe87720
noyuno <noyuno@users.noreply.github.com>
2018-10-21 02:13:49 +0900

    Update

--- postgres/init/00-init.sh
+++ postgres/init/00-init.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
-psql -U postgres -tc "select 1 from user where usename = 'gitbucket'"        | grep -q 1 || psql -U postgres -c "create role gitbucket login password '${POSTGRES_GITBUCKET_PASSWORD}';"
-psql -U postgres -tc "select 1 from pg_database where datname = 'gitbucket'" | grep -q 1 || psql -U postgres -c "create database gitbucket;"
-psql -U postgres -c "grant all privileges on database gitbucket to gitbucket;"
+i() {
+    t=$1
+    p=$2
+    psql -U postgres -tc "select 1 from user where usename = '$t'"        | grep -q 1 || psql -U postgres -c "create role $t login password '$p';"
+    psql -U postgres -tc "select 1 from pg_database where datname = '$t'" | grep -q 1 || psql -U postgres -c "create database $t template template0 encoding 'unicode';"
+    psql -U postgres -c "grant all privileges on database $t to $t;"
+}
+
+i gitbucket "${POSTGRES_GITBUCKET_PASSWORD}"
+i owncloud  "${POSTGRES_OWNCLOUD_PASSWORD}"
 
