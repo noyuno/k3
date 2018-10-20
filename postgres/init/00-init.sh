d5d69008d4643a022eb2e41b4c7a5ccc81de6bca
noyuno <noyuno@users.noreply.github.com>
2018-10-21 02:37:47 +0900

    Update

--- postgres/init/00-init.sh
+++ postgres/init/00-init.sh
@@ -3,7 +3,7 @@
 i() {
     t=$1
     p=$2
-    psql -U postgres -tc "select 1 from user where username = '$t'"        | grep -q 1 || psql -U postgres -c "create role $t login password '$p';"
+    psql -U postgres -tc "select 1 from pg_user where usename = '$t'"        | grep -q 1 || psql -U postgres -c "create role $t login password '$p';"
     psql -U postgres -tc "select 1 from pg_database where datname = '$t'" | grep -q 1 || psql -U postgres -c "create database $t template template0 encoding 'unicode';"
     psql -U postgres -c "grant all privileges on database $t to $t;"
 }
