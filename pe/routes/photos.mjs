d44874456d07ab863846572b724066140249ddc9
noyuno <noyuno@users.noreply.github.com>
2019-09-29 18:35:48 +0900

    multer error handling

--- pe/routes/photos.mjs
+++ pe/routes/photos.mjs
@@ -107,7 +107,14 @@ _router.get('/photos/:directory/:name',
 // upload a image
 _router.post('/photos', 
   pass.authenticate('digest', {session: false}), 
-  multer({ storage: photoStorage }).single('file'), 
+  multer({ storage: photoStorage }).single('file')(req, res, (err) => {
+    if (err) {
+      res.status(500).json({
+        'result': 'error',
+        'error': err
+      })
+    }
+  }), 
   (req, res) => {
     try {
       res.json({ 'result': 'success' });
